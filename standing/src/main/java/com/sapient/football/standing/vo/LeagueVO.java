ce1064811907081d667b87877c2660b4879d2fad
PSEH-7 <39406614+PSEH-7@users.noreply.github.com>
2019-11-22 17:07:05 +0530

    Update LeagueVO.java

--- standing/src/main/java/com/sapient/football/standing/vo/LeagueVO.java
+++ standing/src/main/java/com/sapient/football/standing/vo/LeagueVO.java
@@ -1,5 +1,41 @@
 package com.sapient.football.standing.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+
 public class LeagueVO {
+
+	@JsonProperty("league_id")
+
+	private Integer id;
+
+	@JsonProperty("league_name")
+
+	private String name;
+
+	public Integer getId() {
+
+		return id;
+
+	}
+
+	public void setId(Integer id) {
+
+		this.id = id;
+
+	}
+
+	public String getName() {
+
+		return name;
+
+	}
+
+	public void setName(String name) {
+
+		this.name = name;
+
+	}
 
 }
