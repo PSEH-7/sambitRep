7f17c36115c2ed87ef7d3a15c70c7b1b21b484f6
PSEH-7 <39406614+PSEH-7@users.noreply.github.com>
2019-11-22 17:07:51 +0530

    Update LeagueRepository.java

--- standing/src/main/java/com/sapient/football/standing/repositories/LeagueRepository.java
+++ standing/src/main/java/com/sapient/football/standing/repositories/LeagueRepository.java
@@ -1,5 +1,49 @@
 package com.sapient.football.standing.repositories;
 
+import java.util.Arrays;
+
+
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.stereotype.Repository;
+
+import org.springframework.web.client.RestTemplate;
+
+
+
+import com.sapient.football.standing.vo.LeagueVO;
+
+
+
+@Repository
+
 public class LeagueRepository {
+
+
+
+	@Value("${api-host}")
+
+	private String footballApiHost;
+
+	
+
+	@Value("${api-key}")
+
+	private String apiKey;
+
+	
+
+	public LeagueVO getLeagueByName(String name, Integer countryId) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		String uri = footballApiHost+ "?action=get_leagues&country_id="+ countryId+"&APIkey="+apiKey;
+
+		LeagueVO[] leagueList= restTemplate.getForObject(uri, LeagueVO[].class);
+
+	    return Arrays.stream(leagueList).filter(league -> league.getName().equalsIgnoreCase(name)).findFirst().get();
+
+	}
 
 }
