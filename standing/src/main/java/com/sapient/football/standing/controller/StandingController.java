4f075535d12b027425fbac9b33d1a27f40862f2b
PSEH-7 <39406614+PSEH-7@users.noreply.github.com>
2019-11-22 16:30:41 +0530

    Update StandingController.java

--- standing/src/main/java/com/sapient/football/standing/controller/StandingController.java
+++ standing/src/main/java/com/sapient/football/standing/controller/StandingController.java
@@ -1,5 +1,69 @@
 package com.sapient.football.standing.controller;
 
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.sapient.football.standing.vo.StandingVO;
+
+import com.sapient.football.standing.services.StandingService;
+
+
+
+@RestController
+
+
+
 public class StandingController {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+	
+
+	@Autowired
+
+	private StandingService service;
+
+	@RequestMapping("/standing")
+
+	    public StandingVO greeting(@RequestParam("countryName") String countryName,
+
+	    		                 @RequestParam("leagueName") String leagueName,
+
+	    		                 @RequestParam("teamName") String teamName) {
+
+		
+
+		
+
+	        return service.getStanding(countryName, leagueName, teamName);
+
+	    }
+
+   
+
+	
+
+	@ExceptionHandler
+
+	public String handleException( Exception e ) {
+
+		logger.error(e.getMessage());
+
+		return "Invalid input";
+
+	}
 
 }
